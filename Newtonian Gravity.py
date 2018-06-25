7a5edba39a5f99f2e5e73086d69af6c4e0893916
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-06-25 04:03:24 -0500

    Completed algorithm for first_order function

--- Newtonian Gravity.py
+++ Newtonian Gravity.py
@@ -2,9 +2,17 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 class Newtonian_Gravity():
     '''Simulation of newtonian gravity in 3 dimensions'''
+    #Gravitational constant
+    G = 1
+    #Mass of each particle
+    m = 1
+    #Euler timestep
+    dt = .01
+    
     @staticmethod
     def create_initial_conditions():
         '''Create parameters for particles'''
@@ -43,13 +51,45 @@ class Newtonian_Gravity():
         self.redraw()
         return
 
+    @staticmethod
+    def unit_vec(r1,r2):
+        '''Return a unit vector in the r2-r1 direction'''
+        r = r2-r1
+        magn = np.dot(r,r)
+        return r/math.sqrt(magn)
+
+    @staticmethod
+    def inverse_square(r1,r2):
+        '''Return 1/r^2 where r=r2-r1'''
+        r = r2-r1
+        magn = np.dot(r,r)
+        return 1/magn
+    
+    @staticmethod
+    def weighted_vec(r1,r2):
+        '''Return the vector 1/r^2 in the r direction where r=r2-r1'''
+        r = Newtonian_Gravity.inverse_square(r1,r2)*Newtonian_Gravity.unit_vec(r1,r2)
+        return r
+    
     def first_order(self):
         '''Calculate new parameters for particles with 1st order accuracy'''
         #calculate new positions via r = r_0 + v*dt
+        for i in range(len(self.part_pos)):
+            self.part_pos[i] = self.part_pos[i] + self.part_vel[i]*dt
+            
         #calculate force on ith particle due to j particles
             #don't forget the small parameter to avoid dividing by 0
-        #obtain acceleration from a = F/m
-        #calculate new velocities via v = v_0 + a_dt
+        force = np.array([[0,0,0]]*len(self.part_pos))
+        for i in range(len(self.part_pos)):
+            for j in range(len(self.part_pos)):
+                if (i != j):
+                    #if force is instead repelling, change + to -
+                    force[i] = force[i] + G*m*m*Newtonian_Gravity.weighted_vec(
+                        self.part_pos[i],self.part_pos[j]
+                        )
+        #calculate new velocities via v = v_0 + F/m*dt
+        for i in range(len(self.part_pos)):
+            self.part_vel[i] = self.part_vel[i] + force[i]/m*dt
         return
 
     def second_order(self):
