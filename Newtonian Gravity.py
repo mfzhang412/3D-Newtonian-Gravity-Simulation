834dddfcc59f235e0381df35e5ad870a88f54cc7
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-06-25 01:56:04 -0500

    Created skeleton of program

---
+++ Newtonian Gravity.py
@@ -0,0 +1,45 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.anmation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Newtonian_Gravity():
+    '''Simulation of newtonian gravity in 3 dimensions'''
+
+    @staticmethod
+    def create_initial_conditions():
+        '''Create parameters for particles'''
+        return
+        
+    def __init__(self):
+        '''Create plot for simulation'''
+        return
+
+    def initial_draw(self):
+        '''Draw data onto plot from the initial conditions'''
+        return
+
+    def start_simulation(self):
+        '''Start animation'''
+        return
+
+    def update(self):
+        '''Animated function'''
+        return
+
+    def calculate(self):
+        '''Calculate new parameters for particles'''
+        return
+
+    def redraw(self):
+        '''Redraw plot with new particle parameters'''
+        return
+
+
+
+def main():
+    '''Main function'''
+    return
+
+if __name__ == "__main__":
+    main()
